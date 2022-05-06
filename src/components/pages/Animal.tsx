310c761bba1ebeec9e0597725547fd295855c8fb
AlexanderHansen2 <alexander.hansen@utb.ecutbildning.se>
2022-05-06 11:37:42 +0200

    Local storage

--- src/components/pages/Animal.tsx
+++ src/components/pages/Animal.tsx
@@ -1,8 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { IExtendedAnimal } from "../interface/IExtendedAnimal";
+
 export const Animal = () => {
+    let param = useParams();
+    const [extendedAnimalInfo, setExtendedAnimalInfo] = useState<IExtendedAnimal>()
+
+    useEffect(() => {
+        axios.get("https://animals.azurewebsites.net/api/animals" + param.id)
+        .then((response) => {
+            setExtendedAnimalInfo(response.data)
+        })
+    })
+   const [animalId, setAnimalId] = useState<IExtendedAnimal[]>([]) 
+   
+   let singleAnimal = animalId.map( (local) => {
+       localStorage.getItem("List of animals")
+       return (
+           <>
+           <div>{local.name}</div>
+           </>
+       )
+   } )
+   
 
     return (
         <>
-        <div>Animal work</div>
+        <div>
+            <h3>{singleAnimal}</h3>
+        </div>
         </>
     )
 }
